27ef6cebdc843e20e1dca81a9354626237e39585
snowtoslow <snowtoslow@mail.ru>
2020-09-14 17:45:15 +0300

    add new test version for tree view

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"Security-Benchmarking-Tool/constants"
-	"Security-Benchmarking-Tool/files"
-	"Security-Benchmarking-Tool/store"
 	"Security-Benchmarking-Tool/utils"
 	"fmt"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"log"
 	"os"
@@ -28,7 +27,7 @@ func init() {
 		}
 	}
 }
-
+/*
 func main() {
 
 	HOME, err := utils.GetUserHome()
@@ -88,7 +87,7 @@ func main() {
 	parseButton.Connect("clicked", func() {
 		arrayData := files.ParseFile(policyFileNameNew)
 		info := store.CreateMapForMultipleItems(arrayData)
-		/*fmt.Println(store.SearchItemsByKey(info,"description"))*/
+		fmt.Println(store.SearchItemsByKey(info,"description"))
 		jsonFileName, err := utils.GenerateSavedFileName(auditPath+constants.ParsedDataDirectory, constants.ParsedFileFormat, constants.ParsedPolicy)
 		if err = store.CreateJsonResponse(jsonFileName, info); err != nil {
 			log.Println("ERROR CREATING JSON", err)
@@ -99,7 +98,107 @@ func main() {
 	})
 
 
-	win.ShowAll()
 
+	gtk.Main()
+}*/
+
+
+const (
+	COLUMN_VERSION = iota
+	COLUMN_FEATURE
+)
+
+
+// Add a column to the tree view (during the initialization of the tree view)
+func createColumn(title string, id int) *gtk.TreeViewColumn {
+	cellRenderer, err := gtk.CellRendererTextNew()
+	if err != nil {
+		log.Fatal("Unable to create text cell renderer:", err)
+	}
+
+	column, err := gtk.TreeViewColumnNewWithAttribute(title, cellRenderer, "text", id)
+	if err != nil {
+		log.Fatal("Unable to create cell column:", err)
+	}
+
+	return column
+}
+
+// Creates a tree view and the list store that holds its data
+func setupTreeView() (*gtk.TreeView, *gtk.ListStore) {
+	treeView, err := gtk.TreeViewNew()
+	if err != nil {
+		log.Fatal("Unable to create tree view:", err)
+	}
+
+	treeView.AppendColumn(createColumn("Version", COLUMN_VERSION))
+	treeView.AppendColumn(createColumn("Feature", COLUMN_FEATURE))
+
+	// Creating a list store. This is what holds the data that will be shown on our tree view.
+	listStore, err := gtk.ListStoreNew(glib.TYPE_STRING, glib.TYPE_STRING)
+	if err != nil {
+		log.Fatal("Unable to create list store:", err)
+	}
+	treeView.SetModel(listStore)
+
+	return treeView, listStore
+}
+
+// Append a row to the list store for the tree view
+func addRow(listStore *gtk.ListStore, version, feature string) {
+	// Get an iterator for a new row at the end of the list store
+	iter := listStore.Append()
+
+	// Set the contents of the list store row that the iterator represents
+	err := listStore.Set(iter,
+		[]int{COLUMN_VERSION, COLUMN_FEATURE},
+		[]interface{}{version, feature})
+
+	if err != nil {
+		log.Fatal("Unable to add row:", err)
+	}
+}
+
+// Create and initialize the window
+func setupWindow(title string) *gtk.Window {
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+
+	win.SetTitle(title)
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	win.SetPosition(gtk.WIN_POS_CENTER)
+	win.SetDefaultSize(600, 300)
+	return win
+}
+
+func main() {
+	gtk.Init(nil)
+
+	win := setupWindow("Go Feature Timeline")
+	/*HOME, err := utils.GetUserHome()
+	auditPath := HOME + constants.DESKTOP + constants.AuditDirectory
+	policyFileName, err := utils.GenerateSavedFileName(auditPath+constants.SavedFileDIRECTORY, constants.AuditFormat, constants.Policy)
+	arrayData := files.ParseFile(policyFileName)
+	info := store.CreateMapForMultipleItems(arrayData)*/
+
+
+
+	treeView, listStore := setupTreeView()
+	
+	win.Add(treeView)
+
+	// Add some rows to the list store
+	addRow(listStore, "r57", "Gofix command added for rewriting code for new APIs")
+	addRow(listStore, "r60", "URL parsing moved to new \"url\" package")
+	addRow(listStore, "go1.0", "Rune type introduced as alias for int32")
+	addRow(listStore, "go1.1", "Race detector added to tools")
+	addRow(listStore, "go1.2", "Limit for number of threads added")
+	addRow(listStore, "go1.3", "Support for various BSD's, Plan 9 and Solaris")
+
+	win.ShowAll()
 	gtk.Main()
 }
